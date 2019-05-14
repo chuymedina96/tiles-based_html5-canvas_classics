593a40e55d9997fa020371c21cd16eac92e05ef5
neo <chuymedina96@gmail.com>
2019-05-14 15:54:15 -0500

    more

--- 1st-game/js/main.js
+++ 1st-game/js/main.js
@@ -7,9 +7,9 @@ window.onload = function(){
   canvasContext = canvas.getContext('2d');
 
   canvasContext.fillStyle = 'black';
-  canvasContext.fillRect = (0,0,canvas.width,canvas.height);
+  canvasContext.fillRect = (1,100,canvas.width,canvas.height);
 
-  canvasContext.fillStyle = 'black';
+  canvasContext.fillStyle = 'yellow';
   canvasContext.beginPath();
   canvasContext.arc(100,100,10,0,Math.PI*2, true);
   canvasContext.fill();
